25c66c9ba923adfad0acc37c68d546b0f8df5cd0
disjfa <disjfa@disjfa.nl>
2018-03-29 22:07:40 +0000

    Updates

---
+++ precache-manifest.446bd1a011124c8769547ed126e0d859.js
@@ -0,0 +1,13 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ea0ff85284faf221f0e",
+    "url": "styles/bundle.css"
+  },
+  {
+    "revision": "bbb0934735166c2708a4214671159626",
+    "url": "index.html"
+  },
+  {
+    "url": "bundle.9004d91e4cfa885a64b2.js"
+  }
+];
